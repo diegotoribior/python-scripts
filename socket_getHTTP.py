2e3a79a76d04e3ddd12dbff711c52f63e7c2aba9
Diego Toribio Rodríguez <91145437+diegotoribior@users.noreply.github.com>
2022-01-25 20:43:47 +0000

    Upload socket_getHTTP

---
+++ socket_getHTTP.py
@@ -0,0 +1,16 @@
+import socket
+
+# Se conecta a la url como si fuera un navegador, 
+# mostrando el contenido HTML e información del servidor
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80)) #URL de ejemplo
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
